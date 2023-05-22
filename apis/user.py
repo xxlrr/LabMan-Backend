79420768ef5b6d5025e944e920fd51c0880b55ad
Hongxing Hao <a1845302@adelaide.edu.au>
2023-05-22 21:02:43 +1000

    add user CURD APIs

--- apis/user.py
+++ apis/user.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from apis.auth import get_current_user, authorize
+from models.user import User
+from models import db
+
 
 user = Blueprint('user', __name__)
 
@@ -9,3 +12,55 @@ user = Blueprint('user', __name__)
 def get_user_profile():
     user = get_current_user()
     return jsonify(user)
+
+
+@user.route("/api/users/", methods=["GET"])
+@authorize()
+def get_Users():
+    page = request.args.get("current", None, type=int)
+    page_size = request.args.get("pageSize", None, type=int)
+    username = request.args.get("username", "")
+
+    query = User.query
+    if username:
+        query = query.filter(User.username.like(f"%{username}%"))
+
+    paginated_users = query.paginate(page=page, per_page=page_size, error_out=False)
+    total = paginated_users.total
+    users = paginated_users.items
+
+    return jsonify({
+        "total": total,
+        "list": users,
+    }), 200
+
+@user.route("/api/user/<int:id>/", methods=["GET"])
+@authorize(["Manager"])
+def get_user(id):
+    user = User.query.get(id)
+    return jsonify(user), 200
+
+
+@user.route("/api/user/", methods=["POST"])
+@authorize(["Manager"])
+def add_user():
+    user = User(**request.json)
+    db.session.add(user)
+    db.session.commit()
+    return {}, 200
+
+
+@user.route("/api/user/<int:id>/", methods=["PUT"])
+@authorize(["Manager"])
+def mod_user(id):
+    User.query.filter_by(id=id).update(request.json)
+    db.session.commit()
+    return {}, 200
+
+
+@user.route("/api/user/<int:id>/", methods=["DELETE"])
+@authorize(["Manager"])
+def del_user(id):
+    User.query.filter_by(id=id).delete()
+    db.session.commit()
+    return {}, 200
